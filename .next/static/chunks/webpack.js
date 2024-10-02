3e548cdc3a7eef3d9683befeaf6f274d9557abfb
Rodrigo A <1rodrigo1a@gmail.com>
2024-10-01 22:14:28 -0700

    created parallel files: persons2.json, pages/persons2/[id].js, index2.js, data2.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "105e93660582f1c3"; }
+/******/ 		__webpack_require__.h = function() { return "18afa526a9aec0ba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
