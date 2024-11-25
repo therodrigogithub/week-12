211d97ac6775e590d025505239d1f938a7c9e01f
Rodrigo <1rodrigo1a@gmail.com>
2024-11-25 00:20:21 -0800

    updated data.js and [id].js

--- pages/[id].js
+++ pages/[id].js
@@ -15,7 +15,7 @@ export async function getStaticProps( { params } ) {
 // define a getStaticPaths() function to tell next.js all valid URLs: 1,2,3,4 
 // - this name is defined by next.js
 export async function getStaticPaths() {
-  const paths = getAllIds();
+  const paths = await getAllIds();
   return {
     paths,
     fallback: false
@@ -28,10 +28,9 @@ export default function Entry( { itemData } ) {
     <Layout>
       <article className="card col-6">
         <div className="card-body">
-          <h5 className="card-title">{itemData.name}</h5>
-          <h6 className="card-subtitle mb-2 text-body-secondary">{itemData.phone}</h6>
-          <p className="card-text">{itemData.birthdate}</p>
-          <a href="#" className="card-link">{itemData.email}</a>
+          <h5 className="card-title">{itemData.post_title}</h5>
+          <h6 className="card-subtitle mb-2 text-body-secondary">{itemData.user_login}</h6>
+          <div className="card-text">(itemData.post_content)</div>
         </div>
       </article>
     </Layout>
